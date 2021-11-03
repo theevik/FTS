c20cea600cc664e4a0ced7b2bd45df209ef6e86f
theevik <61742865+theevik@users.noreply.github.com>
2021-11-03 15:03:28 +0100

    door function

---
+++ test/Assets/scripts/openthedoor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openthedoor : MonoBehaviour
+{
+   
+   public void openDoor()
+    {
+        Debug.Log("idk man");
+    }
+}
